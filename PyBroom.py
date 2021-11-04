7b7b284e6cbc3991af8a8c4b1fbfc8eca31f08ee
Pr0gram-Creat0r-1 <80353875+Pr0gram-Creat0r-1@users.noreply.github.com>
2021-11-04 11:35:40 -0700

    Update PyBroom.py

--- PyBroom.py
+++ PyBroom.py
@@ -4,7 +4,21 @@ def clean():
   file_path=input('Paste a file path here: ')
   file=open(file_path)
   text=file.read()
-  lines=text.count('\n')
   the_list=text.splitlines()
+  counter=0
   for x in the_list:
-    pass
+    if the_list[counter].find('=')!=-1:
+      #Identify as a variable assignment
+      id_as_variable=the_list[counter].split('=')
+      variable=id_as_variable[0]
+      subcounter=0
+      prev_find=0
+      for y in the_list:
+        number=the_list[subcounter].find(variable)
+        prev_finds+=number
+        subcounter+=1
+      if prev_finds==1:
+        new_string='\n'.join(the_list)
+        new_string.replace(variable, '')
+        new_string.replace(id_as_variable[1], '')
+    counter+=1
