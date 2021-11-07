3f65e7ca4680b5b36b111db6608b922a69b421a8
Pr0gram-Creat0r-1 <80353875+Pr0gram-Creat0r-1@users.noreply.github.com>
2021-11-06 17:56:52 -0700

    Add files via upload

--- PyBroom.py
+++ PyBroom.py
@@ -7,26 +7,25 @@ def clean():
     text=file.read()
     the_list=text.splitlines()
     counter=0
-    for x in the_list:
+    for x in range(0, len(the_list)):
         if the_list[counter].find('=')!=-1:
             #Identify as a variable assignment
             id_as_variable=the_list[counter].split('=')
             variable=id_as_variable[0]
             exterminator=0
-            previous_finds=0
-            for y in the_list:  
-                if the_list[exterminator].find(variable)!=-1:
+            for y in range(0, len(the_list)):
+                string='\n'.join(the_list)
+                if string.find(variable)!=-1:
                     number=1
                 else:
                     number=0
-                previous_finds+=number
+                if number==1:
+                    new=string.replace(variable, '')
+                    new_string=new.replace(id_as_variable[1], '')
+                    the_list=new_string.splitlines()
                 exterminator+=1
-            if previous_finds==1:
-                new_string='\n'.join(the_list)
-                new_string.replace(variable, '')
-                new_string.replace(id_as_variable[1], '')
         counter+=1
     new_file_name=file_path.replace('.py', '_PyBroom_cleaned.py')
     new_file=open(new_file_name, 'w')
-    new_file.write(new_string)
+    new_file.write(string)
     print('Done!')
