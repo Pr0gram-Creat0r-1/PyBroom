bbc12f46268a0b5ac841170c13eb59bd283ab6e0
Pr0gram-Creat0r-1 <80353875+Pr0gram-Creat0r-1@users.noreply.github.com>
2021-11-07 18:26:17 -0800

    Add files via upload

--- PyBroom.py
+++ PyBroom.py
@@ -10,23 +10,22 @@ def clean():
     for x in range(0, len(the_list)):
         if the_list[counter].count('=')==1:
             #Identify as a variable assignment
+            line=the_list[counter]
             id_as_variable=the_list[counter].split('=')
             variable=id_as_variable[0]
             exterminator=0
             for y in range(0, len(the_list)):
                 string='\n'.join(the_list)
-                if string.count(variable)==1 or (string.count(variable)==2 and (string.count("'%s'" % variable)==1 or string.count('"%s"' % variable)==1 or string.count("'''%s'''" % variable)==1 or string.count('"""%s"""' % variable)==1)):
+                if string.count(variable)==1:
                     number=1
                 else:
                     number=0
                 if number==1:
-                    new=string.replace(variable, '')
-                    add_equal_sign='='+id_as_variable[1]
-                    new_string=new.replace(add_equal_sign, '')
-                    the_list=new_string.splitlines()
+                    new=string.replace(line, '')
+                    the_list=new.splitlines()
                 exterminator+=1
         counter+=1
     new_file_name=file_path.replace('.py', '_PyBroom_cleaned.py')
     new_file=open(new_file_name, 'w')
-    new_file.write('\n'.join(the_list))
+    new_file.write(string)
     print('Done!')
