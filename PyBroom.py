586da127afbf38d0ce188ef341c0f6e8984dc2b6
d10smaradona <therealpengyan@gmail.com>
2021-11-09 20:36:16 -0800

    Reverting to what the repo owner had originally.

--- PyBroom.py
+++ PyBroom.py
@@ -1,41 +1,31 @@
 #I am going to try keeping this file clean
 #Imagine cleaning a code cleaner :)
-
-### There are some obvious issues with the code. I am pointing them out in comment lines starting with 3 #s.
 def clean():
-  file_path=input('Paste a file path here: ')
-  print('Cleaning...')
-  file=open(file_path)
-  text=file.read()
-  the_list=text.splitlines()
-  ### Iterating through a list doesn't really need the "counter". You can simply do "for x in the_list:" and then "x.find('=')!=-1".
-  counter=0 ### You can remove this line. 
-  for x in the_list:
-    if the_list[counter].find('=')!=-1:
-      #Identify as a variable assignment
-      id_as_variable=the_list[counter].split('=')
-      variable=id_as_variable[0]
-      subcounter=0 ### Same thing with the "subcounter". You don't need it to iterate through a list.
-      prev_find=0 ### Typo? You used "prev_finds" later.
-      ### The following "for" loop - I don't understand exactly what you are trying to do with it.
-      ### "prev_finds" is only set to 1 if the string value of "variable" happens to be at index 1 of the line "the_list[blah]".
-      ### If "variable" appears in multiple places, "prev_finds" will keep adding up.
-      ### Anyway, I assume you are trying to implement your goal #1 - remove unused variables. What is an unused variable in the first place?
-      ### A variable that's only initialized but never appears again in the file? I think you may need to re-design the check for that.
-      for y in the_list:
-        number=the_list[subcounter].find(variable)
-        prev_finds+=number
-        subcounter+=1
-      if prev_finds==1:
-        new_string='\n'.join(the_list)
-        new_string.replace(variable, '')
-        new_string.replace(id_as_variable[1], '')
-    counter+=1 ### You can remove this line.
-  new_file_name=file_path.replace('.py', '_PB_cleaned.py')
-  new_file=open(new_file_name, 'w')
-  ### "new_string" is only initialized if prev_finds==1. If that "if" block is not entered, the next line will generate an error.
-  ### You need to initialize this variable in an unconditional place.
-  new_file.write(new_string)
-  print('Done!')
-  
-  ### Even if you fix the simple typos (prev_finds/prev_find, properly initialize "new_string"), the program will still only generate an empty file. Try for yourself.
+    file_path=input('Paste a file path here: ')
+    print('Cleaning...')
+    file=open(file_path)
+    text=file.read()
+    the_list=text.splitlines()
+    counter=0
+    for x in range(0, len(the_list)):
+        if the_list[counter].count('=')==1:
+            #Identify as a variable assignment
+            line=the_list[counter]
+            id_as_variable=the_list[counter].split('=')
+            variable=id_as_variable[0]
+            exterminator=0
+            for y in range(0, len(the_list)):
+                string='\n'.join(the_list)
+                if string.count(variable)==1:
+                    number=1
+                else:
+                    number=0
+                if number==1:
+                    new=string.replace(line, '')
+                    the_list=new.splitlines()
+                exterminator+=1
+        counter+=1
+    new_file_name=file_path.replace('.py', '_PyBroom_cleaned.py')
+    new_file=open(new_file_name, 'w')
+    new_file.write(string)
+    print('Done!')
