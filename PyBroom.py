1931d255627953ce1430522cd2d06b5ad5046518
Pr0gram-Creat0r-1 <80353875+Pr0gram-Creat0r-1@users.noreply.github.com>
2021-11-29 14:44:40 -0800

    Update PyBroom.py

--- PyBroom.py
+++ PyBroom.py
@@ -1,12 +1,33 @@
 #I am going to try keeping this file clean
 #Imagine cleaning a code cleaner :)
-def clean():
+def clean_variables():
     file_path=input('Paste a file path here: ')
     print('Cleaning...')
     file=open(file_path)
     text=file.read()
     the_list=text.splitlines()
     counter=0
+    variable_list=[]
+    word_list=[]
+    real_word_list=[]
+    for x in range(0, len(the_list)):
+      line=the_list[counter]
+      variable=line.split('=')[0]
+      variable_list.append(variable)
+      counter+=1
+    counter=0
+    baby_word_string='\n'.join(the_list)
+    word_string=((((((baby_word_string.replace('(', ' ')).replace(')', ' ')).replace('+', ' ')).replace('-', ' ')).replace('*', ' ')).replace('/', ' ')).replace('=', ' ')
+    word_list=word_string.split('\n')
+    for x in range(0, len(word_list)):
+      string_list=word_list[counter].split(' ')
+      subcounter=0
+      for y in range(0, len(string_list)):
+        real_word_list.append(string_list[subcounter])
+        subcounter+=1
+      counter+=1
+    counter=0
+    #anything beyond this comment I will have to fix.
     for x in range(0, len(the_list)):
         if the_list[counter].count('=')>=1:
             #Identify as a variable assignment
